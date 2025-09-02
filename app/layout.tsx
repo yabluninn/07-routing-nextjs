b073ae4ae13ab54eeab75129924cd8acf6eae176
yabluninn <yablunin.artem@gmail.com>
2025-09-02 12:11:05 +0300

    fix some issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
+import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "NoteHub",
   description: "Note taking app",
 };
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -18,6 +22,7 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
           <main>{children}</main>
+          {modal}
           <Footer />
         </TanStackProvider>
       </body>
